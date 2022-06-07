ad8d8ffe6f4396baa9d1cb9f3830c0519d597b3a
sooonkyu <soonkyu602@gmail.com>
2022-06-07 19:56:26 +0900

    [Clien]수정 CreateWord, Mypage

--- client/src/Pages/CreateWord.js
+++ client/src/Pages/CreateWord.js
@@ -3,6 +3,7 @@ import Nav from "../Component/Nav"
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import "./Mypage.css"
 
 function CreateWord () {
   const userInfo = useSelector((state) => state.userInfo);
@@ -30,68 +31,57 @@ function CreateWord () {
         { users_id, word, summary, content, pub, type },
         {withCredentials: true}
         )
-        .then((res) => {
-          //if (res.data.success){
-          console.log("res", res);
-          navigate("/Mypage")
-          //}
-        //  navigate("/Mypage")
+        .then((data) => {
+          console.log("data", data);
+          navigate("/Mypage", { state: data.data });
         });
       }
   }
-  console.log("id값", id);
-  console.log(Wordcreate);
 
     return (
       <div>
         <Nav />
+      <div className="CreateWord">
         <form onSubmit={(e) => e.preventDefault()}>
           <div className="Word_Create">
-            <span>단어</span>
+            <span>단어</span>&emsp;
             <input className="input_word" type='text' onChange={handleInputValue('word')} />
           </div>
-          <div>
-            <span>요약</span>
+          <div className="Summary_Create">
+            <span>요약</span>&emsp;
             <input className="input_summary" type='text' onChange={handleInputValue('summary')} />
           </div>
           <div>
-            <span>그룹 설정</span>
+            <span>그룹 설정</span>&emsp;
           </div>
-          <div>
-            <span>구분</span>
+          <div className="Type_Create">
+            <span>구분</span>&emsp;
             <input type="radio" name="type" value={"All"} onChange={handleInputValue('type')}/>전체
             <input type="radio" name="type" value={"person"} onChange={handleInputValue('type')}/>인물
             <input type="radio" name="type" value={"place"} onChange={handleInputValue('type')}/>장소
             <input type="radio" name="type" value={"date"} onChange={handleInputValue('type')}/>날짜
           </div>
-          <div className="pub">
+          <div className="Pub_Create">
             <input type="radio" name="open" value={true} onChange={handleInputValue('pub')}/>공개
             <input type="radio" name="open" value={false} onChange={handleInputValue('pub')}/>비공개
           </div>
-          <div>
-            <span>내용</span>
+          <div className="Content_Image">
+            <span>이미지</span>&emsp;
+            <input ref={Wordcreate.image} className="input_content" type='file' accept='image/*' onChange={handleInputValue('image')} />
+          </div>
+          <div className="Content_Create">
+            <span>내용</span>&emsp;
             <input className="input_content" type='text' onChange={handleInputValue('content')} />
           </div>
-          <div className="create_button">
+          <div className="Create_Button">
             <button className="btn" type="button" onClick={handleCreateword}>
               단어 등록하기
             </button>
           </div>
         </form>
       </div>
+      </div>
     )
   }
   
 export default CreateWord
-
-/*
-const [state, setState] = useState([])
-
-  const handlechange = (e) => {
-    console.log(e.target.value);
-    setState(e.target.value)
-  };
-
-<input type="radio" value="false" checked={state === "false"} onChange={handlechange}/>비공개
-
-            */
