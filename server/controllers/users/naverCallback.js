211bea0f68ec7c11932f536745a1887c83dd753b
sooonkyu <soonkyu602@gmail.com>
2022-06-16 10:53:09 +0900

    [Client]수정 : Mypage, Profile, GridCard .js / Mypage,Component,CatFilter .css

--- server/controllers/users/naverCallback.js
+++ server/controllers/users/naverCallback.js
@@ -1,12 +1,13 @@
 const { generateAccessToken, sendAccessToken, removeAccessToken, isAuthorized } = require("../tokenFuntions");
-const axios = require("axios");
+//const axios = require("axios");
 const bcrypt = require("bcrypt");
 const { user } = require("../../models");
 
 module.exports = async (req, res) => {
+
   const { code, state } = req.query;
   if (code && state) {
-    const response = await axios.get(`https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=iwK_gnCquLTp4ZUNXTFs&client_secret=Roe7KhP5NS&code=${code}&state=${state}`).then((res) => res.json());
+     const response = await axios.get(`https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=iwK_gnCquLTp4ZUNXTFs&client_secret=Roe7KhP5NS&code=${code}&state=${state}`).then((res) => res.json());
     const naverAccessToken = response.access_token;
     const naverUserInfo = await axios
       .get(`https://openapi.naver.com/v1/nid/me`, {
