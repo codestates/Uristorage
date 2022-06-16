10d8504bec65c2c335b34f7672bf1db3298f2b1c
yongjin <yonggartong@naver.com>
2022-06-16 18:19:07 +0900

    [Client]추가:markdown적용

--- client/src/Component/GridCards.js
+++ client/src/Component/GridCards.js
@@ -13,14 +13,14 @@ export default function GridCars({ wordData, deleteWord }) {
   const [modalOn, setModalOn] = useState(false);
 
   return (
-    <Col lg={24} md={2} xs={24} >
+    <Col lg={24} md={2} xs={24}>
       {groupFilter === 0 ? ( //내 단어일 경우 수정,삭제 기능 추가
         <div className="gridcards">
           <div className="gridword">
             <div className="gridword_box">
-            <Link to="/Detail" state={{ data: wordData }}>
-              {wordData.word}
-            </Link>
+              <Link to="/Detail" state={{ data: wordData }}>
+                {wordData.word}
+              </Link>
             </div>
           </div>
           {/* <div className="gridnickname">작성자:[{wordData.user.nickname}]</div> */}
@@ -28,30 +28,44 @@ export default function GridCars({ wordData, deleteWord }) {
             <div className="gridsummary_box">{wordData.summary}</div>
           </div>
           <div className="gridchange">
-          <Link to="/ModifyWord" state={{ data: wordData }}>
-            수정
-          </Link>
-          <button className='gridbutton' onClick={() => setModalOn(true)}>삭제</button>
-          <Modal open={modalOn} close={() => setModalOn(false)} wordId={wordData.id} deleteWord={deleteWord} />
+            <Link to="/ModifyWord" state={{ data: wordData }}>
+              수정
+            </Link>
+            <button className="gridbutton" onClick={() => setModalOn(true)}>
+              삭제
+            </button>
+            <Modal open={modalOn} close={() => setModalOn(false)} wordId={wordData.id} deleteWord={deleteWord} />
           </div>
-          <img className="gridimage" src={wordData.image} onError="this.style.display='none'" />
+          <img
+            className="gridimage"
+            src={wordData.image}
+            onError={(e) => {
+              e.target.style.display = "none";
+            }}
+          />
         </div>
       ) : (
         <div className="gridcards">
           <div className="gridword">
             <div className="gridword_box">
-            <Link to="/Detail" state={{ data: wordData }}>
-              {wordData.word}
-            </Link>
+              <Link to="/Detail" state={{ data: wordData }}>
+                {wordData.word}
+              </Link>
             </div>
           </div>
           <div className="gridsummary">
             <div className="gridsummary_box">{wordData.summary}</div>
           </div>
           <div className="gridchange_group">
-          <div className="gridnickname">작성자:[{wordData.user.nickname}]</div>
+            <div className="gridnickname">작성자:[{wordData.user.nickname}]</div>
           </div>
-          <img className="gridimage" src={wordData.image} />
+          <img
+            className="gridimage"
+            src={wordData.image}
+            onError={(e) => {
+              e.target.style.display = "none";
+            }}
+          />
         </div>
       )}
     </Col>
