fba179f9cc15d91c42aa1798b77c73d8e57f9d6b
yongjin <yonggartong@naver.com>
2022-06-19 17:59:01 +0900

    [Client/Server]수정:맵마크,달력

--- client/src/Component/Calendar.js
+++ client/src/Component/Calendar.js
@@ -1,44 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
-import Calendar from 'react-calendar';
-import axios from "axios";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
 import moment from "moment";
-import 'react-calendar/dist/Calendar.css';
-import "./Component.css"
+import "./Component.css";
 
-function Calendarcomponent() {
-  const userInfo = useSelector((state) => state.userInfo);
-  const users_id = userInfo.id;
-  const groupFilter = useSelector((state) => state.groupfilter);
+function Calendarcomponent({ worddata }) {
   const [value, onChange] = useState(new Date());
-  const [calendarWord, setCalendarWord] = useState([]);
-
-  useEffect(() => {
-    async function getCalendarWords() {
-      if (groupFilter === 0) {
-        await axios.get(`${process.env.REACT_APP_URL}/words/user/${users_id}`).then((res) => setCalendarWord(res.data));
-        console.log(res.data);
-      } else {
-        axios.get(`${process.env.REACT_APP_URL}/words/group/${groupFilter}`).then((res) => {
-          console.log(res.data);
-          setCalendarWord(res.data.groupWords);
-        });
-      }
-    }
-    getCalendarWords();
-  }, [groupFilter]);
-
 
   const marks = [];
-  for (let i = 0; i < calendarWord.length; i++) {
-    if (calendarWord[i].type === "date" && calendarWord[i].calendar !== "") {
-      marks.push(calendarWord[i].calendar);
+  for (let i = 0; i < worddata.length; i++) {
+    if (worddata[i].type === "date" && worddata[i].calendar !== "") {
+      marks.push(worddata[i].calendar);
     }
   }
-
-  console.log("marks", marks);
-  console.log("calendarWord", calendarWord);
-
 
   return (
     <div className="highlight">
--- client/src/Component/Locationmap.js
+++ client/src/Component/Locationmap.js
@@ -1,33 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { RenderAfterNavermapsLoaded, NaverMap, Marker } from "react-naver-maps";
-import axios from "axios";
-import { useSelector } from "react-redux";
 
-function Locationmap() {
-  const userInfo = useSelector((state) => state.userInfo);
-  const users_id = userInfo.id;
-  const groupFilter = useSelector((state) => state.groupfilter);
-
-  const [locationWord, setLocationWord] = useState([]);
-
-  useEffect(() => {
-    async function getLocationWords() {
-      if (groupFilter === 0) {
-        await axios.get(`${process.env.REACT_APP_URL}/words/user/${users_id}`).then((res) => setLocationWord(res.data));
-      } else {
-        axios.get(`${process.env.REACT_APP_URL}/words/group/${groupFilter}`).then((res) => {
-          setLocationWord(res.data.groupWords);
-        });
-      }
-    }
-    getLocationWords();
-  }, [groupFilter]);
-
+function Locationmap({ worddata }) {
   const markerInfo = [];
-  for (let i = 0; i < locationWord.length; i++) {
-    if (locationWord[i].type === "place" && locationWord[i].map !== "") {
+  for (let i = 0; i < worddata.length; i++) {
+    if (worddata[i].type === "place" && worddata[i].map !== "") {
       // 두번째 조건은 차후에 삭제
-      markerInfo.push(locationWord[i].map);
+      markerInfo.push(worddata[i].map);
     }
   }
 
@@ -38,25 +17,19 @@ function Locationmap() {
       decodeMarkerInfo.push({ lat: el[0], lng: el[1] });
     }
   }
-  // console.log(decodeMarkerInfo);
 
   const popUpInfo = [];
-  for (let i = 0; i < locationWord.length; i++) {
-    if (locationWord[i].type === "place" && locationWord[i].map !== "") {
+  for (let i = 0; i < worddata.length; i++) {
+    if (worddata[i].type === "place" && worddata[i].map !== "") {
       // 두번째 조건은 차후에 삭제
-      popUpInfo.push([locationWord[i].word, locationWord[i].summary]);
+      popUpInfo.push([worddata[i].word, worddata[i].summary]);
     }
   }
 
   const [infoBelowMap, SetInfoBelowMap] = useState("");
   const clickMarkerHandler = (index) => {
-    // let thisPop = []
-    // thisPop.push(popUpInfo[index][0] + ' - ' + popUpInfo[index][1])
     SetInfoBelowMap(popUpInfo[index][0] + " - " + popUpInfo[index][1]);
-    // console.log(infoBelowMap)
   };
-
-  // const navermap = window.naver.maps;
 
   return (
     <div>
@@ -64,20 +37,13 @@ function Locationmap() {
         <NaverMap
           className="Location_map"
           mapDivId={"naver-map"}
-          defaultCenter={{ lat: 37.3595704, lng: 127.105399 }}
+          defaultCenter={decodeMarkerInfo[0]}
           defaultZoom={16}
           zoomControl={true} // 지도 zoom 허용
           draggable={true}
         >
           {decodeMarkerInfo.map((address, index) => {
-            return (
-              <Marker
-                key={index}
-                position={address}
-                onClick={() => clickMarkerHandler(index)}
-                //animation={navermaps.Animation. DROP}
-              />
-            );
+            return <Marker key={index} position={address} onClick={() => clickMarkerHandler(index)} />;
           })}
         </NaverMap>
         <div className="Map-info">{infoBelowMap}</div>
